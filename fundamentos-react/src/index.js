7e579b5a79d848b5fd8b16dd77a9dd45a40dcc94
kiwm <pedromaltapedrozo@gmail.com>
2021-03-10 17:14:07 -0300

    componentes

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,7 +2,20 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 
-const tag = <strong>Ola React!</strong>
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
-ReactDOM.render(<div> { tag } </div>, document.getElementById('root'))
+
+ReactDOM.render(
+<div> 
+    <Primeiro></Primeiro>
+    <ComParametro
+    titulo="segundo Componente"
+    aluno="Muito legal!"
+    nota="9.3"
+    /> 
+</div>, 
+document.getElementById('root')
+)
+
 
